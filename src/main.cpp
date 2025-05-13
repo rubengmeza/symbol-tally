1a583fdb054e21d4ea2106376877c0bffa576e8f
Ruben <rubengmeza05@gmail.com>
2025-05-12 22:18:44 -0700

    disabled poppler. app will still function as intended

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include <print>
 #include <iostream>
-#include "../include/poppler_pdf_handler.hpp"
+//#include "../include/poppler_pdf_handler.hpp"
 
 #include "../include/app.hpp"
 
+// Poppler does not play nice with windows. Disabled for turn in purposes. Image already generated and app will work as intended.
 int main() 
 {
 	// Convert PDF to an image for manipulation
-	Poppler_Pdf_Handler pdf_handler;
-	pdf_handler.load_pdf("../example/landscape_plans.pdf");
-	pdf_handler.process_pdf();
-	pdf_handler.create_image_from_pdf("../example/planting.png");
+	//Poppler_Pdf_Handler pdf_handler;
+	//pdf_handler.load_pdf("../example/landscape_plans.pdf");
+	//pdf_handler.process_pdf();
+	//pdf_handler.create_image_from_pdf("../example/planting.png");
 
 	App app;
 	app.run();
